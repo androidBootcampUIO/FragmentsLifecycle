a4e3c6df46252df2a23f2ae83b9fa2e65cad3d80
luz <luzma_87@yahoo.com>
2017-10-23 22:37:03 -0500

    imprime logs en la consola y en 2 textviews para mostrar el ciclo de vida de FirstFragment

--- app/src/main/java/uio/androidbootcamp/fragmentslifecycle/FirstFragment.java
+++ app/src/main/java/uio/androidbootcamp/fragmentslifecycle/FirstFragment.java
@@ -2,7 +2,9 @@ package uio.androidbootcamp.fragmentslifecycle;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ public class FirstFragment extends Fragment {
     private String mParam1;
 
     private OnFirstFragmentInteractionListener mListener;
+    private TextView lifecycleLogTextView;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -30,6 +33,7 @@ public class FirstFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        logLifecycleStep("2: [FirstFragment] onCreate()");
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
         }
@@ -37,21 +41,36 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        logLifecycleStep("3: [FirstFragment] onCreateView()");
         View view = inflater.inflate(R.layout.fragment_first, container, false);
         TextView helloFirstFragmentTextView = view.findViewById(R.id.text_view_hello_first_fragment);
+        lifecycleLogTextView = view.findViewById(R.id.text_view_lifecycle_log_fragment);
         helloFirstFragmentTextView.setText(getActivity().getString(R.string.hello_first_fragment, mParam1));
         return view;
     }
 
-    public void onButtonPressed() {
+    public void logOnActivity(String newLog) {
         if (mListener != null) {
-            mListener.onFirstFragmentInteraction();
+            mListener.onFirstFragmentEventLogged(newLog);
         }
+    }
+
+    private void logLifecycleStep(String newText) {
+        String textViewExistsLog = " (el textView fragment aun no existe)";
+        if (lifecycleLogTextView != null) {
+            textViewExistsLog = " (el textView fragment ya existe)";
+            String text = lifecycleLogTextView.getText().toString();
+            text = newText + textViewExistsLog + "\n---\n" + text;
+            lifecycleLogTextView.setText(text);
+        }
+        logOnActivity(newText + textViewExistsLog);
+        Log.e("LIFECYCLE_FRAGMENT_1", newText + textViewExistsLog);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        logLifecycleStep("1: [FirstFragment] onAttach()");
         if (context instanceof OnFirstFragmentInteractionListener) {
             mListener = (OnFirstFragmentInteractionListener) context;
         } else {
@@ -60,13 +79,57 @@ public class FirstFragment extends Fragment {
         }
     }
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        logLifecycleStep("4: [FirstFragment] onActivityCreated()");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        logLifecycleStep("5: [FirstFragment] onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        logLifecycleStep("6: [FirstFragment] onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        logLifecycleStep("7: [FirstFragment] onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        logLifecycleStep("8: [FirstFragment] onStop()");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        logLifecycleStep("9: [FirstFragment] onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        logLifecycleStep("10: [FirstFragment] onDestroy()");
+    }
+
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        logLifecycleStep("11: [FirstFragment] onDetach()");
     }
 
     public interface OnFirstFragmentInteractionListener {
-        void onFirstFragmentInteraction();
+        void onFirstFragmentEventLogged(String newLog);
     }
 }
--- app/src/main/java/uio/androidbootcamp/fragmentslifecycle/MainActivity.java
+++ app/src/main/java/uio/androidbootcamp/fragmentslifecycle/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements
         FirstFragment.OnFirstFragmentInteractionListener {
+
+    private TextView lifecycleLogTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,19 @@ public class MainActivity extends AppCompatActivity implements
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, firstFragment);
         fragmentTransaction.commit();
+
+        lifecycleLogTextView = (TextView) findViewById(R.id.text_view_lifecycle_log_activity);
     }
 
     @Override
-    public void onFirstFragmentInteraction() {
-
+    public void onFirstFragmentEventLogged(String newLog) {
+        String textViewExistsLog = " (el textView activity aun no existe)";
+        if (lifecycleLogTextView != null) {
+            textViewExistsLog = " (el textView activity ya existe)";
+            String text = lifecycleLogTextView.getText().toString();
+            text = newLog + textViewExistsLog + "\n---\n" + text;
+            lifecycleLogTextView.setText(text);
+        }
+        Log.e("LIFECYCLE_DEMO_ACTIVITY", newLog + textViewExistsLog);
     }
 }
